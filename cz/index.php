0cb1910cbe1a471b2be7a6271a85e6f6669f9604
filip316 <110289851+filip316@users.noreply.github.com>
2022-08-08 19:19:44 +0200

    Prvotní zveřejnění mého webu

---
+++ cz/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="cs" class="h-100">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="" />
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" />
+  <meta name="generator" content="Hugo 0.101.0" />
+  <title>Vítej! | Filip</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
+
+  <!-- Custom styles for this template -->
+  <link href="/css/cover.css" rel="stylesheet" />
+</head>
+
+<body class="d-flex h-100 text-center text-bg-dark">
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-auto">
+      <div>
+        <!--
+        <h3 class="float-md-start mb-0">Cover</h3>
+        <nav class="nav nav-masthead justify-content-center float-md-end">
+          <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Home</a>
+          <a class="nav-link fw-bold py-1 px-0" href="#">Features</a>
+          <a class="nav-link fw-bold py-1 px-0" href="#">Contact</a>
+        </nav>
+        -->
+      </div>
+    </header>
+
+    <main class="px-3">
+      <h1>Vítej!</h1>
+      <p class="lead">
+        Jsem začínající programátor webových stránek.<br>
+        Momentálně pracuji na webu Asociace speciálních pedagogů České republiky.
+      </p>
+      <p class="lead">
+        <a href="https://www.aspcr.eu/" target="_blank" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Navštívit</a>
+      </p>
+    </main>
+
+    <footer class="mt-auto text-white-50">
+      <p>
+        &copy;2022 Filip Rund
+      </p>
+    </footer>
+  </div>
+</body>
+
+</html>
